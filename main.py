981a4f6f24ef0e8f53c1389274c89f5928369a3b
Himanshu Babbar <himanshubabbar1806@gmail.com>
2022-10-08 21:53:54 +0530

    edited post.html and post func

--- main.py
+++ main.py
@@ -42,7 +42,15 @@ class Contact(db.Model):
    phone_num = db.Column(db.String(12),nullable=False)
    message = db.Column(db.String(120),nullable=False)
    date = db.Column(db.String(12),nullable=True)
-   
+
+class Posts(db.Model):
+   #sno,name,email,phone_num,message,date
+   sno = db.Column(db.Integer, primary_key = True)
+   title = db.Column(db.String(100),nullable=False) 
+   slug = db.Column(db.String(50),nullable=False)
+   content = db.Column(db.String(255),nullable=False)
+   date = db.Column(db.String(12),nullable=True)
+      
 
 @app.route("/")
 def home():
@@ -57,9 +65,10 @@ def rest():
     data = "hellot this is data"
     return jsonify(data=data)
 
-@app.route("/post")
-def post():
-    return render_template("post.html",params=params)
+@app.route("/post/<string:post_slug>", methods=['GET'])
+def post_route(post_slug):
+    post = Posts.query.filter_by(slug=post_slug)
+    return render_template("post.html",params=params,post=post)
 
 @app.route("/contact",methods=["GET", "POST"])
 def contact():
